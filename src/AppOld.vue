4e096e4102b23dc5e57016ecaf336d0c56fdea62
serenespringbloom <123131645+serenespringbloom@users.noreply.github.com>
2024-12-01 16:52:31 +0800

    Creating Header of Website (#4)

--- src/AppOld.vue
+++ src/AppOld.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <div>
-      <Header />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-  </header> 
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>

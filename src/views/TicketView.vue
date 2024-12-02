1c7427c2f79ac7836a126e466b3afcfab2bf1b53
serenespringbloom <123131645+serenespringbloom@users.noreply.github.com>
2024-12-02 18:27:27 +0800

    Fixed Header Placement And Add New Page Routing

---
+++ src/views/TicketView.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <h1>This is a ticket page</h1>
+    </main>
+  </template>
+    
+  <style>
+  @media (min-width: 1024px) {
+    .ticket {
+      top: 30;
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  

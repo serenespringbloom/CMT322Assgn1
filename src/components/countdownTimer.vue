f05c2e904e0502791cea7e4ccd3b4f0f4a8f60b7
Huda <104578385+hudnabilah@users.noreply.github.com>
2024-12-03 06:49:35 +0800

    Fixed more padding, navbar, homepage (#9)
    * Fixed padding issues, added new files, etc.
    
    Fixed padding issues where the navigation bar and the body leaks through each other. Minimised and maximised window.
    
    Added new view files such as Feedback, Merchandise.
    
    Added MCB logo and put the logo into the navbar.
    
    Fixed the router issue where it loads everytime it goes to another page such as Merchandise, Feedback, etc.
    
    * Dropdown Navbar
    
    Modified the navigation bar when it is minimised, it will become drop down.
    
    * Fixed more padding, navbar, homepage
    
    Changed the navigation bar to drop down in the smaller window version.
    
    Fixed the padding, background, everything in between.
    
    Changed the general text colour to our theme.
    
    Fixed the based and css because there were overlapping and inconsistency. Also App.vue.
    
    Added countdown timer file but on hold because I don't know why it doesnt work.
    
    Decorated homepage in AboutView.
    
    Added background image for home page.

---
+++ src/components/countdownTimer.vue
@@ -0,0 +1,13 @@
+<template>
+  <vue-countdown :time="time" :interval="100" v-slot="{ days, hours, minutes, seconds }">
+    {{ days }} days, {{ hours }} hours, {{ minutes }} minutes, {{ seconds }} seconds.
+  </vue-countdown>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+// Calculate the countdown target time
+const now = new Date().getTime(); // Convert current date to timestamp
+const targetDate = new Date(2024, 5, 1).getTime(); // June 1, 2024 (Months are 0-indexed)
+const time = ref(targetDate - now); // Difference in milliseconds
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueCountdown from "@chenfengyuan/vue-countdown";
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component("VueCountdown", VueCountdown);
 app.mount('#app')

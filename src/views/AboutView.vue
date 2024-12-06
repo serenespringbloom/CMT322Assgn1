bb24ebd91d682c9071db21866dc3337765cc81f7
SYAFIE NAZMI SIDDIQ <111298509+SyafNazmi@users.noreply.github.com>
2024-12-06 14:58:02 +0800

    embedded location (#50)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -258,7 +258,7 @@
     <!-- Map Section -->
     <div class="map-wrapper">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18..."
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.67890123!2d100.3028544!3d5.3616547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304acb0000000000%3A0x1234567890abcdef!2sDewan+Tuanku+Syed+Putra%2C+Universiti+Sains+Malaysia!5e0!3m2!1sen!2smy!4v1700000000000"
         width="600"
         height="350"
         style="border:0;"

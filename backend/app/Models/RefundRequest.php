16817dfbb3e0ea8a823b3e50c4abc4195822c228
Hazim Azman <hazim.azman529@gmail.com>
2025-01-15 00:43:32 +0800

    Update RefundRequest.php

--- backend/app/Models/RefundRequest.php
+++ backend/app/Models/RefundRequest.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class RefundRequest extends Model
 {
     use HasFactory;
-
+    public $timestamps = false;
     protected $table = 'refund_requests';
 
     protected $primaryKey = 'refund_id';
